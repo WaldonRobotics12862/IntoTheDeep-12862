ef9e9b02281b8421d9591a67cb84625a3e1bca9e
Waldon2 <138072041+Vinchenzo1@users.noreply.github.com>
2024-09-18 19:21:50 -0400

    we created a variables file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Variables {
+   public LinearOpMode opMode = null;
+
+   //generic variables
+   public double dWHEELCIRCUMFERENCE = 3.77953 * Math.PI;
+
+   public Variables(LinearOpMode opmode) { this.opMode =  opmode; }
+}
