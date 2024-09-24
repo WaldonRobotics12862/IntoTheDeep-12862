52706e8f1d5703903d3bae60b7d203ffff0c9b3d
Waldon1 <131625203+Waldon1@users.noreply.github.com>
2024-09-23 20:33:52 -0400

    made auton for left side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuton.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
 public final class LeftAuton extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(-34.87, -62.99, Math.toRadians(90));
+        Pose2d beginPose = new Pose2d(-36, -63, Math.toRadians(90));
         SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, beginPose);
         waitForStart();
         Actions.runBlocking(
@@ -18,18 +18,21 @@ public final class LeftAuton extends LinearOpMode {
                         //Deliver Specimen to High Chamber
                           //1. raise lift to high chamber
                           //2. drive to submersible
-                        .splineTo(new Vector2d(x, y, Math.toRadians(90)))
+                       // .splineTo(new Vector2d(-9, -33, Math.toRadians(90)))
                           //3. drop lift to deliver
                           //4. pull pin
                           //5. drop all the way
                         //Go to the 3 yellow samples and deliver to high basket
                           //1. Back up to three yellow samples
+                      //  .lineToY(-50)
+                        //.splineTo(new Vector2d(-42, -25, Math.toRadians(0)))
+                        //.turn(Math.toRadians(90))
                           //2. Pick up yellow sample
                             //Turn on intake
                             //Slowly go into sample until we get it
-                              //Sense sample??
                             //Hand off to delivery side
                           //3. Raise linear slide to deliver sample to high basket
+                          // drive to high basket
                           //4. Lower linear slide back down to ground level
                           //5. Repeat step two until out of yellow samples, then move to "Park"
                         //Park
@@ -43,6 +46,16 @@ public final class LeftAuton extends LinearOpMode {
 //                        .splineTo(new Vector2d(-26.29, -34.87), Math.toRadians(188.70))
 //                        .splineTo(new Vector2d(-39.17, -25.86), Math.toRadians(176.73))
 //                        .splineTo(new Vector2d(-57.84, -57.62), Math.toRadians(225.00))
+                        .splineTo(new Vector2d(-9.00, -33.00), Math.toRadians(90.00))
+                        .splineTo(new Vector2d(-9.34, -53.11), Math.toRadians(91.97))
+                        .splineTo(new Vector2d(-41.31, -26.07), Math.toRadians(180.00))
+                        .splineTo(new Vector2d(-59.55, -57.41), Math.toRadians(225.00))
+                        .splineTo(new Vector2d(-57.62, -32.94), Math.toRadians(90.00))
+                        .splineTo(new Vector2d(-64.49, -58.05), Math.toRadians(263.47))
+                        .splineTo(new Vector2d(-68.78, -35.95), Math.toRadians(94.65))
+                        .splineTo(new Vector2d(-65.13, -57.19), Math.toRadians(265.94))
+                        .splineTo(new Vector2d(-63.63, -60.41), Math.toRadians(0.94))
+                        .splineTo(new Vector2d(-22.86, -11.91), Math.toRadians(0.00))
                         .build());
     }
 }
