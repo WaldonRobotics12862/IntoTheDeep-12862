d288de07e55f1f1ad4692ef89db902c4ea6e411d
PatrickCox <patrick.r.cox@gmail.com>
2024-09-27 15:48:03 -0400

    added in a sample set of code for a continuous rotation servo and a set of lift motors.  Still working out one or two last bugs but getting close.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuton.java
@@ -1,63 +1,177 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
 import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
-@Autonomous(name="LeftAuton")
 
-public final class LeftAuton extends LinearOpMode {
+@Config
+@Autonomous(name="LeftAuton", group="Autonomous")
+
+public class LeftAuton extends LinearOpMode {
+
+    public class Lift {
+        private DcMotorEx liftLeft;
+        private DcMotorEx liftRight;
+
+        public Lift(HardwareMap hardwareMap) {
+            liftLeft = hardwareMap.get(DcMotorEx.class, "liftLeftMotor");
+            liftLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            liftLeft.setDirection(DcMotorEx.Direction.FORWARD);
+
+            liftRight = hardwareMap.get(DcMotorEx.class, "liftRightMotor");
+            liftRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            liftRight.setDirection(DcMotorEx.Direction.FORWARD);
+        }
+
+        public class LiftUp implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                // powers on motor, if it is not on
+                if (!initialized) {
+                    liftLeft.setPower(0.8);
+                    liftRight.setPower(0.8);
+                    initialized = true;
+                }
+
+                // checks lift's current position
+                double posL = liftLeft.getCurrentPosition();
+                double posR = liftRight.getCurrentPosition();
+                packet.put("liftPos", posL);
+                if (posR < 3000.0 & posL < 3000.0) {
+                    // true causes the action to rerun
+                    return true;
+                } else {
+                    // false stops action rerun
+                    liftLeft.setPower(0);
+                    liftRight.setPower(0);
+                    return false;
+                }
+                // overall, the action powers the lift until it surpasses
+                // 3000 encoder ticks, then powers it off
+            }
+        }
+
+        public Action LiftUp() {
+            return new LiftUp();
+        }
+
+        public class LiftDown implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                // powers on motor, if it is not on
+                if (!initialized) {
+                    liftLeft.setPower(-0.8);
+                    liftRight.setPower(-0.8);
+                    initialized = true;
+                }
+
+                // checks lift's current position
+                double posL = liftLeft.getCurrentPosition();
+                double posR = liftRight.getCurrentPosition();
+                packet.put("liftPos", posL);
+                if (posR >10.0 & posL > 10.0) {
+                    // true causes the action to rerun
+                    return true;
+                } else {
+                    // false stops action rerun
+                    liftLeft.setPower(0);
+                    liftRight.setPower(0);
+                    return false;
+                }
+                // overall, the action powers the lift with negative power until it is under
+                // 10 encoder ticks, then powers it off
+            }
+        }
+
+        public Action LiftDown() {
+            return new LiftDown();
+        }
+    }
+
+    public class Intake {
+        private CRServo intake;
+
+        public Intake(HardwareMap hardwareMap) {
+            intake = hardwareMap.get(CRServo.class, "intake");
+        }
+
+        public class RunIntake implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intake.setPower(1.0);
+                return false;
+            }
+        }
+        public Action closeClaw() {
+            return new RunIntake();
+        }
+
+        public class RevIntake implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intake.setPower(-1.0);
+                return false;
+            }
+        }
+        public Action openClaw() {
+            return new RevIntake();
+        }
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
         Pose2d beginPose = new Pose2d(-36, -63, Math.toRadians(90));
         SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, beginPose);
+        Lift Lift = new Lift(hardwareMap);
+        Intake Intake = new Intake(hardwareMap);
+
+        TrajectoryActionBuilder AutonLeft = drive.actionBuilder(beginPose)
+                .splineTo(new Vector2d(-9.00, -33.00), Math.toRadians(90.00))
+                //.splineTo(new Vector2d(-9.34, -53.11), Math.toRadians(91.97))
+                .splineTo(new Vector2d(-41.31, -26.07), Math.toRadians(180.00))
+                .splineTo(new Vector2d(-59.55, -57.41), Math.toRadians(225.00))
+                .splineTo(new Vector2d(-57.62, -32.94), Math.toRadians(90.00))
+                .splineTo(new Vector2d(-64.49, -58.05), Math.toRadians(263.47))
+                .splineTo(new Vector2d(-68.78, -35.95), Math.toRadians(94.65))
+                .splineTo(new Vector2d(-65.13, -57.19), Math.toRadians(265.94))
+                .splineTo(new Vector2d(-63.63, -60.41), Math.toRadians(0.94))
+                .splineTo(new Vector2d(-22.86, -11.91), Math.toRadians(0.00));
+
+
+
+        //actions that need to happen on init (if any)
+
+
+
         waitForStart();
+        if (isStopRequested()) return;
+
         Actions.runBlocking(
-                drive.actionBuilder(beginPose)
-                        //Deliver Specimen to High Chamber
-                          //1. raise lift to high chamber
-                          //2. drive to submersible
-                       // .splineTo(new Vector2d(-9, -33, Math.toRadians(90)))
-                          //3. drop lift to deliver
-                          //4. pull pin
-                          //5. drop all the way
-                        //Go to the 3 yellow samples and deliver to high basket
-                          //1. Back up to three yellow samples
-                      //  .lineToY(-50)
-                        //.splineTo(new Vector2d(-42, -25, Math.toRadians(0)))
-                        //.turn(Math.toRadians(90))
-                          //2. Pick up yellow sample
-                            //Turn on intake
-                            //Slowly go into sample until we get it
-                            //Hand off to delivery side
-                          //3. Raise linear slide to deliver sample to high basket
-                          // drive to high basket
-                          //4. Lower linear slide back down to ground level
-                          //5. Repeat step two until out of yellow samples, then move to "Park"
-                        //Park
-                          //1. Drive over to ascension zone
-                          //2. Move linear slide to touch low bar
-
-
-
-
-//                        .splineTo(new Vector2d(-7.62, -39.81), Math.toRadians(90.00))
-//                        .splineTo(new Vector2d(-26.29, -34.87), Math.toRadians(188.70))
-//                        .splineTo(new Vector2d(-39.17, -25.86), Math.toRadians(176.73))
-//                        .splineTo(new Vector2d(-57.84, -57.62), Math.toRadians(225.00))
-                        .splineTo(new Vector2d(-9.00, -33.00), Math.toRadians(90.00))
-                        //.splineTo(new Vector2d(-9.34, -53.11), Math.toRadians(91.97))
-                        .splineTo(new Vector2d(-41.31, -26.07), Math.toRadians(180.00))
-                        .splineTo(new Vector2d(-59.55, -57.41), Math.toRadians(225.00))
-                        .splineTo(new Vector2d(-57.62, -32.94), Math.toRadians(90.00))
-                        .splineTo(new Vector2d(-64.49, -58.05), Math.toRadians(263.47))
-                        .splineTo(new Vector2d(-68.78, -35.95), Math.toRadians(94.65))
-                        .splineTo(new Vector2d(-65.13, -57.19), Math.toRadians(265.94))
-                        .splineTo(new Vector2d(-63.63, -60.41), Math.toRadians(0.94))
-                        .splineTo(new Vector2d(-22.86, -11.91), Math.toRadians(0.00))
-                        .build());
+                new SequentialAction(
+                        AutonLeft,
+                        Lift.LiftUp()
+                )
+        );
     }
 }
