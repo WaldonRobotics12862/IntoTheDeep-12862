d73e1032c2de0fa7a703da2ad7cfe0f7bb4d6f59
PatrickCox <patrick.r.cox@gmail.com>
2024-09-30 18:34:53 -0400

    Added the DiveActions file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiveActions.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class DiveActions {
+    public class Lift {
+        private DcMotorEx liftLeft;
+        private DcMotorEx liftRight;
+
+        public Lift(HardwareMap hardwareMap) {
+            liftLeft = hardwareMap.get(DcMotorEx.class, "liftLeftMotor");
+            liftLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            liftLeft.setDirection(DcMotorEx.Direction.FORWARD);
+
+            liftRight = hardwareMap.get(DcMotorEx.class, "liftRightMotor");
+            liftRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+            liftRight.setDirection(DcMotorEx.Direction.FORWARD);
+        }
+
+        public class LiftUp implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                // powers on motor, if it is not on
+                if (!initialized) {
+                    liftLeft.setPower(0.8);
+                    liftRight.setPower(0.8);
+                    initialized = true;
+                }
+
+                // checks lift's current position
+                double posL = liftLeft.getCurrentPosition();
+                double posR = liftRight.getCurrentPosition();
+                packet.put("liftPos", posL);
+                if (posR < 3000.0 & posL < 3000.0) {
+                    // true causes the action to rerun
+                    return true;
+                } else {
+                    // false stops action rerun
+                    liftLeft.setPower(0);
+                    liftRight.setPower(0);
+                    return false;
+                }
+                // overall, the action powers the lift until it surpasses
+                // 3000 encoder ticks, then powers it off
+            }
+        }
+
+        public Action LiftUp() {
+            return new LeftAuton.Lift.LiftUp();
+        }
+
+        public class LiftDown implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                // powers on motor, if it is not on
+                if (!initialized) {
+                    liftLeft.setPower(-0.8);
+                    liftRight.setPower(-0.8);
+                    initialized = true;
+                }
+
+                // checks lift's current position
+                double posL = liftLeft.getCurrentPosition();
+                double posR = liftRight.getCurrentPosition();
+                packet.put("liftPos", posL);
+                if (posR >10.0 & posL > 10.0) {
+                    // true causes the action to rerun
+                    return true;
+                } else {
+                    // false stops action rerun
+                    liftLeft.setPower(0);
+                    liftRight.setPower(0);
+                    return false;
+                }
+                // overall, the action powers the lift with negative power until it is under
+                // 10 encoder ticks, then powers it off
+            }
+        }
+
+        public Action LiftDown() {
+            return new LeftAuton.Lift.LiftDown();
+        }
+    }
+}
