68426783e814f0ffbf16afbada0c5013bb2e3fcb
PatrickCox <patrick.r.cox@gmail.com>
2024-10-15 08:44:50 -0400

    did just some formating clean-up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaldonTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaldonTeleOp.java
@@ -18,78 +18,76 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 public class WaldonTeleOp extends LinearOpMode {
     @Override
         public void runOpMode() throws InterruptedException {
-            // Declare our motors
-            // Make sure your ID's match your configuration
-            DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class,"leftFront");
-            DcMotorEx backLeftMotor = hardwareMap.get(DcMotorEx.class,"leftBack");
-            DcMotorEx frontRightMotor = hardwareMap.get(DcMotorEx.class,"rightBack");
-            DcMotorEx backRightMotor = hardwareMap.get(DcMotorEx.class,"rightFront");
-            IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotorEx frontLeftMotor = hardwareMap.get(DcMotorEx.class,"leftFront");
+        DcMotorEx backLeftMotor = hardwareMap.get(DcMotorEx.class,"leftBack");
+        DcMotorEx frontRightMotor = hardwareMap.get(DcMotorEx.class,"rightBack");
+        DcMotorEx backRightMotor = hardwareMap.get(DcMotorEx.class,"rightFront");
+        IMU imu = hardwareMap.get(IMU.class, "imu");
 
         DiveActions.SpecimenDelivery SpecimenDelivery = new DiveActions.SpecimenDelivery(hardwareMap);
         DiveActions.Intake Intake = new DiveActions.Intake(hardwareMap);
+
         // Reverse the right side motors. This may be wrong for your setup.
-            // If your robot moves backwards when commanded to go forwards,
-            // reverse the left side instead.
-            // See the note about this earlier on this page.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
         frontRightMotor.setDirection(DcMotorEx.Direction.FORWARD);
         backRightMotor.setDirection(DcMotorEx.Direction.FORWARD);
         backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeftMotor.setDirection(DcMotorEx.Direction.FORWARD);
 
         // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.RIGHT));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
 
-            IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                    RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                    RevHubOrientationOnRobot.UsbFacingDirection.RIGHT));
-            // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-            imu.initialize(parameters);
+        waitForStart();
 
-            waitForStart();
+        if (isStopRequested()) return;
 
-            if (isStopRequested()) return;
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
 
-            while (opModeIsActive()) {
-                double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-                double x = gamepad1.left_stick_x;
-                double rx = gamepad1.right_stick_x;
-
-                // This button choice was made so that it is hard to hit on accident,
-                // it can be freely changed based on preference.
-                // The equivalent button is start on Xbox-style controllers.
-                if (gamepad1.a) {
-                    imu.resetYaw();
-                }
-                if (gamepad2.right_bumper){
-                    Actions.runBlocking(new SequentialAction(DiveActions.SpecimenDelivery.open()));
-                }
-                if (gamepad2.left_bumper){
-                    Actions.runBlocking(new SequentialAction(DiveActions.SpecimenDelivery.close()));
-                }
-                if (gamepad2.y){
-                    Actions.runBlocking(new SequentialAction(DiveActions.Intake.ExtendArm()));
-                }
-                double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-                // Rotate the movement direction counter to the bot's rotation
-                double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-                double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-                rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-                // Denominator is the largest motor power (absolute value) or 1
-                // This ensures all the powers maintain the same ratio,
-                // but only if at least one is out of the range [-1, 1]
-                double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-                double frontLeftPower = (rotY + rotX + rx) / denominator;
-                double backLeftPower = (rotY - rotX + rx) / denominator;
-                double frontRightPower = (rotY + rotX - rx) / denominator;
-                double backRightPower = (rotY - rotX - rx) / denominator;
-
-                frontLeftMotor.setPower(frontLeftPower);
-                backLeftMotor.setPower(backLeftPower);
-                frontRightMotor.setPower(frontRightPower);
-                backRightMotor.setPower(backRightPower);
+            if (gamepad1.a) {
+                imu.resetYaw();
             }
+
+            if (gamepad2.right_bumper){
+                Actions.runBlocking(new SequentialAction(DiveActions.SpecimenDelivery.open()));
+            }
+            if (gamepad2.left_bumper){
+                Actions.runBlocking(new SequentialAction(DiveActions.SpecimenDelivery.close()));
+            }
+            if (gamepad2.y){
+                Actions.runBlocking(new SequentialAction(DiveActions.Intake.ExtendArm()));
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY + rotX - rx) / denominator;
+            double backRightPower = (rotY - rotX - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
         }
+    }
 }
