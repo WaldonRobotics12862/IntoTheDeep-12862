5a5537736b38987422be49638a684a8faf6773a7
PatrickCox <patrick.r.cox@gmail.com>
2024-10-30 19:25:06 -0400

    added the reverse motor direction portion to the teleop code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaldonTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WaldonTeleOp.java
@@ -6,6 +6,7 @@ import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -49,6 +50,11 @@ public class WaldonTeleOp extends LinearOpMode {
                 RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
 
         imu.initialize(parameters);
+
+        frontLeftMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorEx.Direction.FORWARD);
 
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
